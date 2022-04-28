9c33d405c1e1eae8156510dde794a6c92c502c0a
뱀 <54323618+viyamKUN@users.noreply.github.com>
2022-04-29 02:03:04 +0900

    타겟 텍스트의 material를 바꾸는 기능 구현

---
+++ Assets/Scripts/UI/MaterialInfoBoxManager.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Yurei.PTMP.UI
+{
+    public class MaterialInfoBoxManager : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI _targetText;
+        [SerializeField]
+        private List<FontMaterial> _fontMaterials;
+
+        [System.Serializable]
+        private struct FontMaterial
+        {
+            public MATERIAL_TYPE type;
+            public Material material;
+        }
+
+        public enum MATERIAL_TYPE
+        {
+            BASE, OUTLINE, GLOW, TEXTURE
+        }
+
+        public void ChangeMaterial(string targetType)
+        {
+            var materialType = targetType.ToUpper();
+            var material = _fontMaterials.Find(x => x.type.ToString().Equals(materialType)).material;
+            _targetText.material = material;
+        }
+    }
+}
